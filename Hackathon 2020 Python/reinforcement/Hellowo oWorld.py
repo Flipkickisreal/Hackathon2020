5e756831e2c24914704580b0029007c23b5b7a4b
Flipkickisreal <42154453+Flipkickisreal@users.noreply.github.com>
2020-04-16 19:58:07 -0400

    started codebase

---
+++ Hackathon 2020 Python/reinforcement/Hellowo oWorld.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 18:46:16 2020
+
+@author: User
+"""
+#Hello World is a good piece of code to start with. 
+#If you are observant it will teach you a lot about code.
+#For example it teaches you the use of parnthases in syntax.
+print('Hello World')
